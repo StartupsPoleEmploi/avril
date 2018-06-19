6e016262958ce375ceb33cdbbb516e8fb8e97f7a
nresni <nresnikow@gmail.com>
2018-06-19 09:00:20 +0200

    [Certification] Fix typo for one or more certifications

--- web/views/certification_view.ex
+++ web/views/certification_view.ex
@@ -14,4 +14,12 @@ defmodule Vae.CertificationView do
   def formatted_name(certification) do
     "#{certification.acronym} #{String.downcase(certification.label)}"
   end
+
+  def render("_certification_title.html", %{count: count} = assigns) when count == 0 do
+    render("certification_title_no_result.html", assigns)
+  end
+
+  def render("_certification_title", assigns) do
+    render("certification_title.html", assigns)
+  end
 end
