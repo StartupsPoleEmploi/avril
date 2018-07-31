82553191467768b758b95dbdc0c2119c3a745fda
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2018-07-31 16:17:17 +0200

    Feature/delegate duplication (#12)

--- web/models/process.ex
+++ web/models/process.ex
@@ -4,32 +4,35 @@ defmodule Vae.Process do
   alias __MODULE__
 
   alias Vae.Delegate
+  alias Vae.Process
   alias Vae.Repo.NewRelic, as: Repo
 
   schema "processes" do
-    field :name, :string
-    field :booklet_1, :string
-    field :booklet_2, :string
+    field(:name, :string)
+    field(:booklet_1, :string)
+    field(:booklet_2, :string)
 
-    field :step_1, :string
-    field :step_2, :string
-    field :step_3, :string
-    field :step_4, :string
-    field :step_5, :string
-    field :step_6, :string
-    field :step_7, :string
-    field :step_8, :string
+    field(:step_1, :string)
+    field(:step_2, :string)
+    field(:step_3, :string)
+    field(:step_4, :string)
+    field(:step_5, :string)
+    field(:step_6, :string)
+    field(:step_7, :string)
+    field(:step_8, :string)
 
-    has_many :delegates, Vae.Delegate, on_replace: :nilify, on_delete: :nilify_all
+    has_many(:delegates, Vae.Delegate, on_replace: :nilify, on_delete: :nilify_all)
 
-    has_many :processes_steps, Vae.ProcessStep, on_delete: :delete_all
-    has_many :steps, through: [:processes_steps, :step]
-
+    has_many(:processes_steps, Vae.ProcessStep, on_delete: :delete_all)
+    has_many(:steps, through: [:processes_steps, :step])
   end
 
   def changeset(process, params \\ %{}) do
     process
-    |> cast(params, ~w(name booklet_1 booklet_2 step_1 step_2 step_3 step_4 step_5 step_6 step_7 step_8))
+    |> cast(
+      params,
+      ~w(name booklet_1 booklet_2 step_1 step_2 step_3 step_4 step_5 step_6 step_7 step_8)
+    )
     |> validate_required([:name])
     |> unique_constraint(:name)
   end
@@ -44,11 +47,35 @@ defmodule Vae.Process do
     changeset
     |> put_assoc(
       :delegates,
-      delegates |> ensure_not_nil |> transform_destroy |> retrieve_delegates |> Enum.uniq_by(&(&1.id))
+      delegates |> ensure_not_nil |> transform_destroy |> retrieve_delegates
+      |> Enum.uniq_by(& &1.id)
     )
   end
 
   def add_delegates(changeset, _no_delegates_param), do: changeset
+
+  def duplicate(process) do
+    duplicate =
+      process
+      |> Map.take([
+        :name,
+        :booklet_1,
+        :booklet_2,
+        :step_1,
+        :step_2,
+        :step_3,
+        :step_4,
+        :step_5,
+        :step_6,
+        :step_7,
+        :step_8
+      ])
+      |> Map.update(:name, "", fn name -> "#{name}_copy" end)
+
+    changeset(%Process{}, duplicate)
+    |> put_assoc(:delegates, process.delegates)
+    |> Repo.insert()
+  end
 
   defp ensure_not_nil(delegates) do
     delegates
@@ -57,10 +84,10 @@ defmodule Vae.Process do
 
   defp transform_destroy(collection_with_destroy) do
     collection_with_destroy
-    |> Enum.reduce([], fn { _, d }, acc ->
+    |> Enum.reduce([], fn {_, d}, acc ->
       case d[:_destroy] do
         "0" -> [d[:id] | acc]
-        _   -> acc
+        _ -> acc
       end
     end)
   end
@@ -68,6 +95,6 @@ defmodule Vae.Process do
   defp retrieve_delegates(delegate_ids) do
     Delegate
     |> where([d], d.id in ^delegate_ids)
-    |> Repo.all
+    |> Repo.all()
   end
 end
