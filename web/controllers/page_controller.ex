9fd0b2a0a2de71a406f7debe34b9fcfaa07c97d0
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2018-08-22 16:56:52 +0200

    Add style on navbar

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,20 +1,15 @@
 defmodule Vae.PageController do
   use Vae.Web, :controller
 
-  alias Vae.Certification
-
   def index(conn, _params) do
-    top_certifications = Certification
-                         |> Certification.top_three
-                         |> Repo.all
-    render conn, "index.html", layout: {Vae.LayoutView, "home.html"}, top_certifications: top_certifications
+    render(conn, "index.html", layout: {Vae.LayoutView, "home.html"})
   end
 
   def vae(conn, _params) do
-    render conn, "vae.html"
+    render(conn, "vae.html", layout: {Vae.LayoutView, "home-white.html"})
   end
 
   def terms_of_use(conn, _params) do
-    render conn, "terms_of_use.html"
+    render(conn, "terms_of_use.html", layout: {Vae.LayoutView, "home-white.html"})
   end
 end
