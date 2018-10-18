e933fd29a529bd5019663c115355dc3dc939a684
Massiani David <davidmassianirennes@gmail.com>
2018-10-18 17:03:48 +0200

    Page "Bien choisir son diplôme" (#29)
    * create page
    
    * content integration
    
    * corrections
    
    * fix single quote
    
    * fix partial scss import
    
    * link to need help to choose your certification, remove pagination when < 1 page
    
    * add google optimize
    
    * fix env key

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -12,4 +12,8 @@ defmodule Vae.PageController do
   def terms_of_use(conn, _params) do
     render(conn, "terms_of_use.html", layout: {Vae.LayoutView, "home-white.html"})
   end
+
+  def choose_certification(conn, _params) do
+    render(conn, "choose-certification.html", layout: {Vae.LayoutView, "home-white.html"})
+  end
 end
