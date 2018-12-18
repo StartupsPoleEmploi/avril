97ff09b394568f39082707330485c4b196567f4b
nresni <nresnikow@gmail.com>
2018-12-18 13:16:44 +0100

    [Search] Fix search query

--- lib/vae/search/client/algolia.ex
+++ lib/vae/search/client/algolia.ex
@@ -6,28 +6,65 @@ defmodule Vae.Search.Client.Algolia do
   def get_delegates(certifiers, geoloc) do
     query =
       init()
+      |> build_active_filter()
       |> build_certifier_filter(certifiers)
       |> build_geoloc(geoloc)
+      |> build_query()
 
     execute(:delegate, query)
   end
 
-  defp init(), do: []
+  def init(), do: %{filters: %{and: [], or: []}, query: [], aroundLatLng: []}
 
-  defp build_certifier_filter(query, certifiers) do
-    filter =
-      certifiers
-      |> Enum.map(&"certifiers=#{&1.id}")
-      |> Enum.join(" OR ")
-
-    [{:filters, filter} | query]
+  def build_active_filter(query) do
+    add_and_filter(query, "is_active=true")
   end
 
-  defp build_geoloc(query, %{"lat" => lat, "lng" => lng}) when nil not in [lat, lng] do
-    [{:aroundLatLng, [lat, lng]} | query]
+  def build_certifier_filter(query, certifiers) do
+    Enum.reduce(certifiers, query, fn certifier, acc ->
+      add_or_filter(acc, "certifiers=#{certifier.id}")
+    end)
   end
 
-  defp build_geoloc(query, _), do: query
+  def build_geoloc(query, %{"lat" => lat, "lng" => lng} = geo) when nil not in [lat, lng] do
+    add_aroundLatLng(query, geo)
+  end
+
+  def build_geoloc(query, _), do: query
+
+  def build_query(query) do
+    build_filters(query) ++ build_geo(query)
+  end
+
+  defp add_and_filter(query, filter) do
+    add_filter(query, {:and, filter})
+  end
+
+  defp add_or_filter(query, filter) do
+    add_filter(query, {:or, filter})
+  end
+
+  defp add_filter(query, {conjunction, filter}) do
+    update_in(query, [:filters, conjunction], fn existing_filter ->
+      [filter | existing_filter]
+    end)
+  end
+
+  defp add_aroundLatLng(query, %{"lat" => lat, "lng" => lng}) do
+    update_in(query, [:aroundLatLng], fn _ -> [lat, lng] end)
+  end
+
+  defp build_filters(%{filters: []} = query), do: query
+
+  defp build_filters(%{filters: %{or: or_filter, and: and_filter}}) do
+    [filters: "(#{Enum.join(or_filter, " OR ")}) AND (#{Enum.join(and_filter, " AND ")})"]
+  end
+
+  defp build_geo(%{aroundLatLng: []} = query), do: query
+
+  defp build_geo(%{aroundLatLng: lat_lng}) do
+    [aroundLatLng: lat_lng]
+  end
 
   defp execute(:delegate, query) do
     Algolia.search("delegate", "", query)
---
+++ test/lib/search/client/algolia_test.ex
@@ -0,0 +1,105 @@
+defmodule Vae.Search.Client.AlgoliaTest do
+  use ExUnit.Case
+
+  alias Vae.Search.Client.Algolia
+  alias Vae.Certifier
+
+  test "build is_active filter" do
+    query = %{
+      filters: %{
+        and: [
+          "is_active=true"
+        ],
+        or: []
+      },
+      query: [],
+      aroundLatLng: []
+    }
+
+    assert query == Algolia.init() |> Algolia.build_active_filter()
+  end
+
+  test "build certifiers filter" do
+    query = %{
+      filters: %{
+        and: [],
+        or: ["certifiers=1", "certifiers=2", "certifiers=3"]
+      },
+      query: [],
+      aroundLatLng: []
+    }
+
+    certifiers = [
+      %Certifier{id: 1},
+      %Certifier{id: 2},
+      %Certifier{id: 3}
+    ]
+
+    assert query[:filters][:or] --
+             (Algolia.init()
+              |> Algolia.build_certifier_filter(certifiers)
+              |> get_in([:filters, :or])) == []
+  end
+
+  test "build geoloc" do
+    query = %{
+      filters: %{
+        and: [],
+        or: []
+      },
+      query: [],
+      aroundLatLng: ["48.866667", "2.333333"]
+    }
+
+    assert query ==
+             Algolia.init() |> Algolia.build_geoloc(%{"lat" => "48.866667", "lng" => "2.333333"})
+  end
+
+  test "full filter built" do
+    query = %{
+      filters: %{
+        and: [
+          "is_active=true"
+        ],
+        or: ["certifiers=1", "certifiers=2", "certifiers=3"]
+      },
+      query: [],
+      aroundLatLng: ["48.866667", "2.333333"]
+    }
+
+    certifiers = [
+      %Certifier{id: 1},
+      %Certifier{id: 2},
+      %Certifier{id: 3}
+    ]
+
+    built_query =
+      Algolia.init()
+      |> Algolia.build_active_filter()
+      |> Algolia.build_certifier_filter(certifiers)
+      |> Algolia.build_geoloc(%{"lat" => "48.866667", "lng" => "2.333333"})
+
+    assert get_in(query, [:filters, :or]) -- get_in(built_query, [:filters, :or]) == []
+    assert get_in(query, [:filters, :and]) == get_in(built_query, [:filters, :and])
+    assert get_in(query, [:aroundLatLng]) == get_in(built_query, [:aroundLatLng])
+  end
+
+  test "build query" do
+    certifiers = [
+      %Certifier{id: 1},
+      %Certifier{id: 2},
+      %Certifier{id: 3}
+    ]
+
+    built_query =
+      Algolia.init()
+      |> Algolia.build_active_filter()
+      |> Algolia.build_certifier_filter(certifiers)
+      |> Algolia.build_geoloc(%{"lat" => "48.866667", "lng" => "2.333333"})
+
+    [
+      aroundLatLng: ["48.866667", "2.333333"],
+      filters: "(certifiers=1 OR certifiers=2 OR certifiers=3) AND (is_active=true)"
+    ] == Algolia.build_query(built_query)
+  end
+end
