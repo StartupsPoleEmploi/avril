dac5a29b2768abf91099c0caa2db5f9a985ab911
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-03-26 17:06:11 +0100

    login protected

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,22 +1,12 @@
 defmodule Vae.UserController do
   require Logger
   use Vae.Web, :controller
+  plug Coherence.Authentication.Session, protected: true
 
   alias Vae.User
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
-    redirect_or_show(conn, user)
-  end
-
-  defp redirect_or_show(conn, nil) do
-    conn
-    |> put_flash(:error, "Vous n'avez pas accès.")
-    |> redirect(to: root_path(conn, :index))
-  end
-
-  defp redirect_or_show(conn, user) do
     render(conn, "show.html", user: user, layout: {Vae.LayoutView, "home-white.html"})
   end
-
 end
