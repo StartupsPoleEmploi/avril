6c7ae1d86533bce2d97b2f16a7ae623e0067aa57
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-04-16 12:09:47 +0200

    fix

--- config/heroku.exs
+++ config/heroku.exs
@@ -36,7 +36,7 @@ config :vae,
   },
   mailjet_template_error_reporting:
     List.first(
-      Enum.map(String.split(System.get_env("MAILJET_PUBLIC_API_KEY"), ","), &%{Email: &1})
+      Enum.map(String.split(System.get_env("MAILJET_EMAIL_OVERRIDE"), ","), &%{Email: &1})
     ),
   mailjet_template_error_deliver: true
 
