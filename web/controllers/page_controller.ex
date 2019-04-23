bebfa1a40262d55d881c636e4dae209856fff66e
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-04-23 14:52:12 +0200

    title better management

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -10,12 +10,14 @@ defmodule Vae.PageController do
 
   def vae(conn, _params) do
     render(conn, "vae.html",
+      title: "Qu'est-ce que la VAE ?",
       body_class: 'home-white'
     )
   end
 
   def terms_of_use(conn, _params) do
     render(conn, "terms_of_use.html",
+      title: "Conditions générales d’utilisation",
       body_class: 'home-white'
     )
   end
@@ -28,24 +30,28 @@ defmodule Vae.PageController do
 
   def accessibility_promess(conn, _params) do
     render(conn, "accessibility-promess.html",
+      title: "Avril et l'accessibilité",
       body_class: 'home-white'
     )
   end
 
   def point_relais_conseil(conn, _params) do
     render(conn, "point-relais-conseil.html",
+      title: "Les Points Relais Conseil VAE",
       body_class: 'home-white'
     )
   end
 
   def certificateur_vae_definition(conn, _params) do
     render(conn, "certificateur-vae-definition.html",
+      title: "C'est quoi un certificateur VAE ?",
       body_class: 'landing-extend'
     )
   end
 
   def pourquoi_une_certification(conn, _params) do
     render(conn, "pourquoi-une-certification.html",
+      title: "Pourquoi un VAE ?",
       body_class: 'landing-extend'
     )
   end
