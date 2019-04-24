192e201d79295de11e5b35757bc019133802bd84
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-04-24 16:26:55 +0200

    ebert feedback

--- web/static/js/analytics.js
+++ web/static/js/analytics.js
@@ -1,28 +1,30 @@
+/*global ga, $ */
+
 var handleAnalyticsEvent = function(target) {
-  if (!window.ga) return console.warn("Analytics not set up:", target);
-  if (!target) return console.warn("Target not correctly set");
+  if (!window.ga) return console.warn('Analytics not set up:', target);
+  if (!target) return console.warn('Target not correctly set');
   if (target.indexOf('?') === 0) {
     var queryString = naiveDeparam(window.location.search).concat(naiveDeparam(target)).join('&');
     ga('send', 'pageview', window.location.pathname + '?' + queryString);
   } else if (target.indexOf('/') === 0) {
     ga('send', 'pageview', target);
   } else {
-    var cat_event = target.split('#');
-    ga('send', 'event', cat_event[0], cat_event[1]);
+    var categoryEvent = target.split('#');
+    ga('send', 'event', categoryEvent[0], categoryEvent[1]);
   }
 }
 
 $(function() {
 
-  $("[data-analytics]").each(function(i, el){
-    if ($(el).is("form")) {
-      $(el).on("submit", function(e){
+  $('[data-analytics]').each(function(i, el){
+    if ($(el).is('form')) {
+      $(el).on('submit', function(e){
         handleAnalyticsEvent($(e.delegateTarget).attr('data-analytics'))
-      })
+      });
     } else {
-      $(el).on("click", function(e){
+      $(el).on('click', function(e){
         handleAnalyticsEvent($(e.delegateTarget).attr('data-analytics'))
-      })
+      });
     }
   });
 
@@ -42,24 +44,24 @@ $(function() {
   //   ga('send', 'event', 'Delegates', 'website');
   // });
 
-  $("#steps :contains('réunion') a").on("click", function() {
-    handleAnalyticsEvent('Delegates#information')
+  $("#steps :contains('réunion') a").on('click', function() {
+    handleAnalyticsEvent('Delegates#information');
   });
 
-  $("#steps :contains('ivret 1') a").on("click", function() {
-    handleAnalyticsEvent('Delegates#livret 1')
+  $("#steps :contains('ivret 1') a").on('click', function() {
+    handleAnalyticsEvent('Delegates#livret 1');
   });
 
-  $("#steps :contains('ivret 2') a").on("click", function() {
-    handleAnalyticsEvent('Delegates#livret 2')
+  $("#steps :contains('ivret 2') a").on('click', function() {
+    handleAnalyticsEvent('Delegates#livret 2');
   });
 
-  $("#steps a[href^='https://candidat.pole-emploi.fr']").on("click", function() {
-    handleAnalyticsEvent('Delegates#espace personnel')
+  $("#steps a[href^='https://candidat.pole-emploi.fr']").on('click', function() {
+    handleAnalyticsEvent('Delegates#espace personnel');
   });
 
 
-  if (window.location.hash.indexOf("ga=") > -1 ) {
+  if (window.location.hash.indexOf('ga=') > -1 ) {
     var command = window.location.hash.replace(/^#?ga=/, '')
     window.location.hash = '';
     handleAnalyticsEvent(command);
