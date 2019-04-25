6a2e826d2f0b842b4e5f06acfaee76334c87d7cc
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-04-25 12:13:45 +0200

    changed to mix task

--- lib/mix/tasks/application/asp_emails.ex
+++ lib/mix/tasks/application/asp_emails.ex
@@ -1,13 +1,13 @@
-defmodule Vae.Repo.Migrations.SubmitAspApplications do
-  use Ecto.Migration
+defmodule Mix.Tasks.Application.AspEmails do
+  use Mix.Task
+
+  import Mix.Ecto
   import Ecto.Query, only: [from: 2]
 
-  def up do
-    Application.ensure_all_started(:vae)
+  def run(_args) do
+    {:ok, _} = Application.ensure_all_started(:vae)
+
     query = from a in Vae.Application, where: is_nil(a.submitted_at)
     Enum.map(Vae.Repo.all(query), &Vae.Application.maybe_autosubmit/1)
-  end
-
-  def down do
   end
 end
