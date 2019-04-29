cf2ecc4ab5a4dbeeb1640c2f18bd96464edeb2c6
nresni <nresnikow@gmail.com>
2019-04-29 18:55:06 +0200

    [Analytics] Fix my :hankey:

--- web/static/js/analytics.js
+++ web/static/js/analytics.js
@@ -1,14 +1,15 @@
 class Analytics {
   init() {
-    let analytics = this
+    let self = this;
+
     $('[data-analytics]').each(function(i, el){
       if ($(el).is('form')) {
         $(el).on('submit', function(e){
-          analytics.handleAnalyticsEvent($(e.delegateTarget).attr('data-analytics'))
+          self.handleAnalyticsEvent($(e.delegateTarget).attr('data-analytics'), self)
         });
       } else {
         $(el).on('click', function(e){
-          analytics.handleAnalyticsEvent($(e.delegateTarget).attr('data-analytics'))
+          self.handleAnalyticsEvent($(e.delegateTarget).attr('data-analytics'), self)
         });
       }
     });
@@ -16,12 +17,12 @@ class Analytics {
     $('.request-contact form').submit(function() {
       const contactData = $(this).serializeJSON();
       if (contactData.contact_delegate) {
-        analytics.handleAnalyticsEvent('Delegates#contact');
+        self.handleAnalyticsEvent('Delegates#contact', self);
       } else {
-        analytics.handleAnalyticsEvent('Steps#email');
+        self.handleAnalyticsEvent('Steps#email', self);
       }
       if (contactData.booklet_1) {
-        analytics.handleAnalyticsEvent('Steps#dossier inscription');
+        self.handleAnalyticsEvent('Steps#dossier inscription', self);
       }
     });
 
@@ -30,34 +31,34 @@ class Analytics {
     // });
 
     $("#steps :contains('réunion') a").on('click', function() {
-      analytics.handleAnalyticsEvent('Delegates#information');
+      self.handleAnalyticsEvent('Delegates#information', self);
     });
 
     $("#steps :contains('ivret 1') a").on('click', function() {
-      analytics.handleAnalyticsEvent('Delegates#livret 1');
+      self.handleAnalyticsEvent('Delegates#livret 1', self);
     });
 
     $("#steps :contains('ivret 2') a").on('click', function() {
-      analytics.handleAnalyticsEvent('Delegates#livret 2');
+      self.handleAnalyticsEvent('Delegates#livret 2', self);
     });
 
     $("#steps a[href^='https://candidat.pole-emploi.fr']").on('click', function() {
-      analytics.handleAnalyticsEvent('Delegates#espace personnel');
+      self.handleAnalyticsEvent('Delegates#espace personnel', self);
     });
 
 
     if (window.location.hash.indexOf('ga=') > -1 ) {
       var command = window.location.hash.replace(/^#?ga=/, '')
       window.location.hash = '';
-      analytics.handleAnalyticsEvent(command);
+      self.handleAnalyticsEvent(command, self);
     }
   }
 
-  handleAnalyticsEvent(target) {
+  handleAnalyticsEvent(target, self) {
     if (!window.ga) return console.warn('Analytics not set up:', target);
     if (!target) return console.warn('Target not correctly set');
     if (target.indexOf('?') === 0) {
-      var queryString = naiveDeparam(window.location.search).concat(naiveDeparam(target)).join('&');
+      var queryString = self.naiveDeparam(window.location.search).concat(self.naiveDeparam(target)).join('&');
       window.ga('send', 'pageview', window.location.pathname + '?' + queryString);
     } else if (target.indexOf('/') === 0) {
       window.ga('send', 'pageview', target);
@@ -66,6 +67,10 @@ class Analytics {
       window.ga('send', 'event', categoryEvent[0], categoryEvent[1]);
     }
   }
-};
+  
+  naiveDeparam(queryString) {
+    return queryString.replace(/^\??/, '').split('&')
+  }
+}
 
 export { Analytics };
