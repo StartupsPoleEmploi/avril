db142037ad62a50b04b75f0835b217d0e0a4e845
nresni <nresnikow@gmail.com>
2019-05-03 12:45:05 +0200

    [CRM] emails callback

---
+++ lib/vae/crm/config.ex
@@ -0,0 +1,32 @@
+defmodule Vae.CRM.Config do
+  def get_monthly_template_id do
+    get_monthly_config()
+    |> get_template_id()
+  end
+
+  def get_monthly_form_urls() do
+    get_monthly_config()
+    |> get_form_urls()
+  end
+
+  defp get_form_urls(config) do
+    config
+    |> get_users_config()
+    |> Keyword.get(:form_urls)
+  end
+
+  defp get_template_id(config) do
+    config
+    |> get_users_config()
+    |> Keyword.get(:template_id)
+  end
+
+  defp get_monthly_config() do
+    Keyword.get(Application.get_env(:vae, :reminders), :monthly)
+  end
+
+  defp get_users_config(config) do
+    config
+    |> Keyword.get(:users)
+  end
+end
--- lib/vae/crm/transactional/monthly.ex
+++ lib/vae/crm/transactional/monthly.ex
@@ -2,9 +2,9 @@ defmodule Vae.CRM.Transactional.Monthly do
   alias Vae.Application, as: JsApplication
 
   alias Vae.Mailer.Email
+  alias Vae.CRM.Config
 
   @sender Application.get_env(:vae, :sender)
-  @reminders Application.get_env(:vae, :reminders)
 
   def execute(date \\ Date.utc_today()) do
     get_applications_from(date)
@@ -30,9 +30,9 @@ defmodule Vae.CRM.Transactional.Monthly do
           custom_id: Atom.to_string(custom_id),
           job_seeker: application.user.job_seeker,
           vars: %{
-            form_url: define_form_url_from_application(application)
+            application_id: application.id
           },
-          template_id: get_template_id()
+          template_id: Config.get_monthly_template_id()
         }
         | acc
       ]
@@ -47,50 +47,5 @@ defmodule Vae.CRM.Transactional.Monthly do
         _ -> [email | acc]
       end
     end)
-  end
-
-  def define_form_url_from_application(application) do
-    certifiers = application.delegate.certifiers
-    get_form_url_from_certifier_id(hd(certifiers).id)
-  end
-
-  defp get_template_id() do
-    get_users_config()
-    |> Keyword.get(:template_id)
-  end
-
-  defp get_monthly_config() do
-    Keyword.get(@reminders, :monthly)
-  end
-
-  defp get_users_config() do
-    get_monthly_config()
-    |> Keyword.get(:users)
-  end
-
-  defp get_form_urls() do
-    get_users_config()
-    |> Keyword.get(:form_urls)
-  end
-
-  defp get_form_url_from_certifier_id(certifier_id) do
-    with form_urls <- get_form_urls(),
-         certifiers_form_url <- Keyword.get(form_urls, :certifiers) do
-      Enum.reduce(
-        certifiers_form_url,
-        get_default_form_url(),
-        fn {_certifier_name, %{ids: ids, url: url}}, acc ->
-          if certifier_id in ids do
-            url
-          else
-            acc
-          end
-        end
-      )
-    end
-  end
-
-  defp get_default_form_url() do
-    get_in(get_form_urls(), [:certifiers, :other, :url])
   end
 end
