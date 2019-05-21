ca4864ce6180174faab065ebfc9eb668498c723a
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-05-21 15:51:13 +0200

    redirect if not admin

--- web/admin/auth.ex
+++ web/admin/auth.ex
@@ -7,13 +7,16 @@ defmodule Vae.CheckAdmin do
   def call(conn, _opts) do
     current_user = Coherence.current_user(conn)
 
-    if current_user.is_admin do
-      conn
+    if current_user do
+      if current_user.is_admin do
+        conn
+      else
+        conn
+        |> put_flash(:error, "Vous n'avez pas accès.")
+        |> redirect(to: "/")
+      end
     else
-      conn
-      |> put_flash(:error, "Vous n'avez pas accès.")
-      # |> put_status(:not_found)
-      |> halt
+      halt
     end
   end
 end
