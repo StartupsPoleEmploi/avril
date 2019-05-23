30e031263e3ec9e7900644776092b43a2118d2b2
nresni <nresnikow@gmail.com>
2019-05-23 18:08:19 +0200

    [CRM] Crm stock reminder from 2018-09-01 to 2019-03-31

--- lib/vae/crm/config.ex
+++ lib/vae/crm/config.ex
@@ -1,5 +1,5 @@
 defmodule Vae.Crm.Config do
-  def get_monthly_template_id do
+  def get_monthly_template_id() do
     get_monthly_config()
     |> get_template_id()
   end
@@ -7,6 +7,16 @@ defmodule Vae.Crm.Config do
   def get_monthly_form_urls() do
     get_monthly_config()
     |> get_form_urls()
+  end
+
+  def get_stock_form_urls() do
+    get_stock_config()
+    |> get_form_urls()
+  end
+
+  def get_stock_template_id() do
+    get_stock_config()
+    |> get_template_id()
   end
 
   defp get_form_urls(config) do
@@ -23,6 +33,10 @@ defmodule Vae.Crm.Config do
 
   defp get_monthly_config() do
     Keyword.get(Application.get_env(:vae, :reminders), :monthly)
+  end
+
+  defp get_stock_config() do
+    Keyword.get(Application.get_env(:vae, :reminders), :stock)
   end
 
   defp get_users_config(config) do
--- web/router.ex
+++ web/router.ex
@@ -14,11 +14,13 @@ defmodule Vae.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+
     plug(Coherence.Authentication.Session,
       store: Coherence.CredentialStore.Session,
       db_model: @user_schema,
       id_key: @id_key
     )
+
     #    plug(Vae.Tracker)
   end
 
@@ -28,6 +30,7 @@ defmodule Vae.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+
     plug(Coherence.Authentication.Session,
       protected: true,
       store: Coherence.CredentialStore.Session,
@@ -82,6 +85,9 @@ defmodule Vae.Router do
     # Loggued in applications
     get("/candidatures/:id/admissible", ApplicationController, :admissible)
     get("/candidatures/:id/inadmissible", ApplicationController, :inadmissible)
+
+    get("/candidats/:id/admissible", JobSeekerController, :admissible)
+    get("/candidats/:id/inadmissible", JobSeekerController, :inadmissible)
 
     resources("/candidatures", ApplicationController, only: [:show, :update]) do
       get("/telecharger", ApplicationController, :download, as: :download)
