7d613ca860678b85264ae8586ad4a05ddd604066
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-06-04 12:10:21 +0200

    youpi ça marche

--- web/models/resume.ex
+++ web/models/resume.ex
@@ -1,6 +1,8 @@
 defmodule Vae.Resume do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Vae.Repo
+
 
   schema "resumes" do
     field :content_type, :string
@@ -24,36 +26,42 @@ defmodule Vae.Resume do
     filename = "#{UUID.uuid4(:hex)}#{Path.extname(params.filename)}"
     {:ok, binary} = File.read(params.path)
 
-    IO.inspect(Application.get_env(:ex_aws, :bucket_name))
-    IO.inspect(filename)
-
     result =
-      ExAws.S3.put_object(Application.get_env(:ex_aws, :bucket_name), filename, binary)
-      |> ExAws.request
+      ExAws.S3.put_object(
+        Application.get_env(:ex_aws, :bucket_name),
+        __MODULE__.to_s3_path(application.id, filename),
+        binary,
+        [
+          content_type: params.content_type,
+          content_disposition: "attachment; filename=#{params.filename}"
+        ]
+      ) |> ExAws.request
 
     case result do
       {:ok, body} ->
-        Repo.insert(__MODULE__.changeset(%{
+        Repo.insert(__MODULE__.changeset(%__MODULE__{}, %{
           application: application,
           content_type: params.content_type,
           filename: params.filename,
-          url: "https://#{Application.get_env(:ex_aws, :bucket_name)}.s3.amazonaws.com/#{Application.get_env(:ex_aws, :bucket_name)}/#{filename}"
+          url: "https://#{Application.get_env(:ex_aws, :bucket_name)}.s3.#{Application.get_env(:ex_aws, :region)}.amazonaws.com/#{Application.get_env(:ex_aws, :bucket_name)}#{__MODULE__.to_s3_path(application.id, filename)}"
         }))
       error -> error
     end
-
-    IO.inspect(result)
-
   end
 
   def delete(resume) do
     result =
-      ExAws.S3.delete_object(Application.get_env(:ex_aws, :bucket_name), List.last(String.split(resume.url, "/")))
+      ExAws.S3.delete_object(Application.get_env(:ex_aws, :bucket_name), __MODULE__.to_s3_path(resume))
       |> ExAws.request
 
-    IO.inspect(result)
-
-    Repo.delete(resume)
+    case result do
+      {:ok, body} ->
+        Repo.delete(resume)
+      error -> error
+    end
   end
+
+  def to_s3_path(application_id, filename), do: "/#{application_id}/#{filename}"
+  def to_s3_path(%__MODULE__{application_id: application_id, url: url}), do: to_s3_path(application_id, List.last(String.split(url, "/")))
 
 end
