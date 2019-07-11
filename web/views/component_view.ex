805300673577585c17e7b64cfb895f1099a85b80
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-07-11 14:48:45 +0200

    analytics debug mode

--- web/views/component_view.ex
+++ web/views/component_view.ex
@@ -41,7 +41,7 @@ defmodule Vae.ComponentView do
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        })(window,document,'script','https://www.google-analytics.com/analytics#{if Mix.env == :dev, do: "_debug"}.js','ga');
 
         ga('create', '#{System.get_env("GA_API_KEY")}', 'auto');
       """,
