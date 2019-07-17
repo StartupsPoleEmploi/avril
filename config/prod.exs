cef85c19d32619512630d665a8d65956d27b050c
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-07-17 18:36:44 +0200

    trailing commas greee

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :vae, Vae.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("WHOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/staging.exs
+++ config/staging.exs
@@ -9,7 +9,7 @@ config :vae, Vae.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("WHOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :debug
 
