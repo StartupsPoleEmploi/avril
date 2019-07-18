ff4eeb8945c16cdf227a69e90689bfdbf5193efe
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-07-18 11:40:39 +0200

    removed warnings

--- web/controllers/admin/api_controller.ex
+++ web/controllers/admin/api_controller.ex
@@ -1,5 +1,5 @@
 defimpl Jason.Encoder, for: [Tuple] do
-  def encode(data, opts) when is_tuple(data) do
+  def encode(data, _opts) when is_tuple(data) do
     {{y, m, d}, {h, min, s, ms}} = data
     Jason.encode!(NaiveDateTime.to_iso8601(%NaiveDateTime{
       year: y,
@@ -24,27 +24,27 @@ defmodule ExAdmin.ApiController do
 
   def delegates_query() do
     """
-select
-  q.certifier_name,
-  q.delegate_name,
-  q.total,
-  q.submitted,
-  (100 * q.submitted / NULLIF(total, 0)) as submitted_percent,
-  q.admissible,
-  q.inadmissible,
-  (q.admissible + q.inadmissible) * 100 / NULLIF(q.submitted, 0) as responded_percent,
-  q.admissible * 100 / NULLIF(q.admissible + q.inadmissible, 0) as admissible_percent
-from (
-  select certifiers.name as certifier_name, delegates.name as delegate_name,
-  (select count(*) from applications where applications.delegate_id = delegates.id) as total,
-  (select count(*) from applications where applications.delegate_id = delegates.id  and applications.submitted_at IS NOT NULL) as submitted,
-  (select count(*) from applications where applications.delegate_id = delegates.id  and applications.admissible_at IS NOT NULL) as admissible,
-  (select count(*) from applications where applications.delegate_id = delegates.id  and applications.inadmissible_at IS NOT NULL) as inadmissible
-  from delegates
-  inner join certifiers_delegates on delegates.id = certifiers_delegates.delegate_id
-  inner join certifiers on certifiers_delegates.certifier_id = certifiers.id
-) q
-order by admissible_percent desc NULLS LAST, total desc
+    select
+      q.certifier_name,
+      q.delegate_name,
+      q.total,
+      q.submitted,
+      (100 * q.submitted / NULLIF(total, 0)) as submitted_percent,
+      q.admissible,
+      q.inadmissible,
+      (q.admissible + q.inadmissible) * 100 / NULLIF(q.submitted, 0) as responded_percent,
+      q.admissible * 100 / NULLIF(q.admissible + q.inadmissible, 0) as admissible_percent
+    from (
+      select certifiers.name as certifier_name, delegates.name as delegate_name,
+      (select count(*) from applications where applications.delegate_id = delegates.id) as total,
+      (select count(*) from applications where applications.delegate_id = delegates.id  and applications.submitted_at IS NOT NULL) as submitted,
+      (select count(*) from applications where applications.delegate_id = delegates.id  and applications.admissible_at IS NOT NULL) as admissible,
+      (select count(*) from applications where applications.delegate_id = delegates.id  and applications.inadmissible_at IS NOT NULL) as inadmissible
+      from delegates
+      inner join certifiers_delegates on delegates.id = certifiers_delegates.delegate_id
+      inner join certifiers on certifiers_delegates.certifier_id = certifiers.id
+    ) q
+    order by admissible_percent desc NULLS LAST, total desc
     """
   end
 
