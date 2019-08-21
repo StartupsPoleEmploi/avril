8dad0e2beff6d5ead7db48ee741f3cd09c945b14
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-08-21 16:02:35 +0200

    scraper and genserver working

---
+++ lib/vae/delegates/afpa/scraper.ex
@@ -0,0 +1,113 @@
+defmodule Vae.Delegates.Afpa.Scraper do
+  require Logger
+
+  def scrape_all_events(page\\0) do
+    case scrape_page_events(page) do
+      [] -> []
+      events when is_list(events) -> events ++ scrape_all_events(page+1)
+      _ -> []
+    end
+  end
+
+  def scrape_page_events(page\\0, nb_per_page\\12) do
+    Logger.info "Scraping page #{page}"
+    query_params = %{
+      p_p_id: "101_INSTANCE_agenda",
+      _101_INSTANCE_agenda_afpa_ddm__22997__DateDebut_en_US: Timex.format!(Timex.today, "%d/%m/%Y", :strftime),
+      _101_INSTANCE_agenda_afpa_ddmStructureKey: "EVENEMENT",
+      _101_INSTANCE_agenda_categoryId: 58_334_180,
+      _101_INSTANCE_agenda_afpa_start: page * nb_per_page,
+      _101_INSTANCE_agenda_afpa_rows: nb_per_page
+    }
+
+    case HTTPoison.get("https://www.afpa.fr/agenda?#{URI.encode_query(query_params)}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+        |> Floki.find(".evenement figcaption .lirelasuite")
+        |> Floki.attribute("id")
+        |> Enum.map(fn id -> scrape_event("https://www.afpa.fr/agenda/#{id}") end)
+        |> Enum.filter(fn meeting -> !is_nil(meeting) end)
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        Logger.warn "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error reason
+    end
+  end
+
+  def scrape_event(url) do
+    Logger.info "Scraping #{url}"
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        custom_back = Floki.find(body, ".custom-back")
+        info_node_children = Floki.find(custom_back, ".center-info") |> node_children()
+        time_node_children = Floki.find(custom_back, ".date-info span:last-child") |> node_children()
+
+        if is_nil(info_node_children) || is_nil(time_node_children) do
+          nil
+        else
+          [name, address, postal_city] =
+            info_node_children
+
+            |> filter_nodes_by_tag_name("br")
+            |> Enum.map(fn node ->
+              Floki.text(node)
+              |> String.replace(~r/\r|\n|\t/, "")
+              |> String.replace(~r/\s+/, " ")
+              |> String.trim()
+            end)
+
+          [postal_code, city] = String.split(postal_city, " ", parts: 2)
+
+          [date, time] =
+            time_node_children
+            |> filter_nodes_by_tag_name("br")
+            |> Enum.map(fn node ->
+              Floki.text(node)
+              |> String.replace(~r/\r|\n|\t/, "")
+              |> String.replace(~r/\s+/, " ")
+              |> String.trim()
+            end)
+
+          %{
+            datetime: format_date(date, time),
+            name: name,
+            address: address,
+            postal_code: String.to_integer(postal_code),
+            city: city
+          } |> Map.merge(%{geolocation: Vae.Places.Client.Algolia.get_geoloc_from_postal_code(String.to_integer(postal_code))})
+        end
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        Logger.warn "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error reason
+    end
+  end
+
+
+  defp node_children(node_list) do
+    case List.first(node_list) do
+      node when is_tuple(node) -> elem(node, 2)
+      _ -> nil
+    end
+  end
+
+  defp filter_nodes_by_tag_name(node_list, tag_name), do: node_list |> Enum.filter(
+    fn
+      node when is_tuple(node) -> elem(node, 0) != tag_name
+      node -> true
+    end
+  )
+  defp format_date(french_date, time) do
+    %{"number" => number, "month_name" => month_name} = Regex.named_captures(~r/[a-z]+ (?<number>\d+) (?<month_name>\D+)/, String.downcase(french_date))
+    [hours, minutes] = String.split(time, ":", parts: 2)
+    Timex.parse!("#{String.pad_leading(number, 2, "0")}/#{french_month_to_integer(month_name)}/#{Timex.today.year} #{String.pad_leading(hours, 2, "0")}:#{String.pad_leading(minutes, 2, "0")}", "%d/%m/%Y %H:%M", :strftime)
+  end
+
+  defp french_month_to_integer(french_month) do
+    months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+    Enum.find_index(months, fn fm -> fm == french_month end) + 1
+    |> Integer.to_string
+    |> String.pad_leading(2, "0")
+  end
+end
--- lib/vae/delegates/afpa/server.ex
+++ lib/vae/delegates/afpa/server.ex
@@ -3,6 +3,8 @@ defmodule Vae.Delegates.Afpa.Server do
   use GenServer
 
   @name Afpa
+  @ets_table :afpa_meetings
+  @ets_key :meetings
 
   @doc false
   def start_link(opts) do
@@ -12,137 +14,51 @@ defmodule Vae.Delegates.Afpa.Server do
   @impl true
   def init(delegate) do
     Logger.info("Init #{delegate} server")
-
-    state = %{
-      name: delegate,
-      data: []
-    }
-
-    {:ok, state, {:continue, :get_data}}
+    PersistentEts.new(@ets_table, "#{@ets_table}.tab", [
+      :set,
+      :named_table,
+      :protected
+      # read_concurrency: true,
+      # write_concurrency: true
+    ])
+    if length(get_meetings()) == 0 do
+      do_refresh()
+    end
+    {:ok, nil}
   end
 
   @impl true
-  def handle_continue(:get_data, state) do
-    data = scrape_calendar()
-    updated_state = Map.put(state, :data, data)
-
-    {:noreply, updated_state}
+  def handle_call(:get_meetings, _from, state) do
+    {:reply, get_meetings(), state}
   end
 
-  # def handle_call(:get_academies, _from, state) do
-  #   {:reply, FranceVae.get_academies(), state}
-  # end
-
-  # def handle_call({:get_meetings, academy_id}, _from, state) do
-  #   {:reply, FranceVae.get_meetings(academy_id), state}
-  # end
-
-  # def handle_call({:post_meeting_registration, academy_id, meeting_id, user}, _from, state) do
-  #   {:reply, FranceVae.post_meeting_registration(academy_id, meeting_id, user), state}
-  # end
-
-  def scrape_multiple_pages(nb_of_pages\\5) do
-    # TODO
+  @impl true
+  def handle_info(:refresh, state) do
+    do_refresh()
+    {:noreply, state}
   end
 
-  def scrape_calendar(page\\0, nb_per_page\\12) do
-    query_params = %{
-      p_p_id: "101_INSTANCE_agenda",
-      _101_INSTANCE_agenda_afpa_ddm__22997__DateDebut_en_US: Timex.format!(Timex.today, "%d/%m/%Y", :strftime),
-      _101_INSTANCE_agenda_afpa_ddmStructureKey: "EVENEMENT",
-      _101_INSTANCE_agenda_categoryId: 58334180,
-      _101_INSTANCE_agenda_afpa_start: page * nb_per_page,
-      _101_INSTANCE_agenda_afpa_rows: nb_per_page
-    }
+  @impl true
+  def handle_cast(:refresh, state) do
+    do_refresh()
+    {:noreply, state}
+  end
 
-    case HTTPoison.get("https://www.afpa.fr/agenda?#{URI.encode_query(query_params)}") do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        body
-        |> Floki.find(".evenement figcaption .lirelasuite")
-        |> Floki.attribute("id")
-        |> Enum.map(fn id -> scrape_event("https://www.afpa.fr/agenda/#{id}") end)
-        |> Enum.filter(fn meeting -> !is_nil(meeting) end)
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+  defp get_meetings() do
+    case :ets.lookup(@ets_table, @ets_key) do
+      [{@ets_key, meetings}] -> meetings
+      [] -> []
     end
   end
 
-  def scrape_event(url) do
-    IO.puts "Scraping #{url}"
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        custom_back = Floki.find(body, ".custom-back")
-        info_node_children = Floki.find(custom_back, ".center-info") |> node_children()
-        time_node_children = Floki.find(custom_back, ".date-info span:last-child") |> node_children()
-
-        if is_nil(info_node_children) || is_nil(time_node_children) do
-          nil
-        else
-          [name, address, postal_city] =
-            info_node_children
-
-            |> filter_nodes_by_tag_name("br")
-            |> Enum.map(fn node ->
-              Floki.text(node)
-              |> String.replace(~r/\r|\n|\t/, "")
-              |> String.replace(~r/\s+/, " ")
-              |> String.trim()
-            end)
-
-          [postal_code, city] = String.split(postal_city, " ", parts: 2)
-
-          [date, time] =
-            time_node_children
-            |> filter_nodes_by_tag_name("br")
-            |> Enum.map(fn node ->
-              Floki.text(node)
-              |> String.replace(~r/\r|\n|\t/, "")
-              |> String.replace(~r/\s+/, " ")
-              |> String.trim()
-            end)
-
-          %{
-            datetime: format_date(date, time),
-            name: name,
-            address: address,
-            postal_code: String.to_integer(postal_code),
-            city: city
-          }
-        end
-
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
-    end
+  defp do_refresh() do
+    :ets.insert(@ets_table, {@ets_key, Vae.Delegates.Afpa.Scraper.scrape_all_events()})
+    schedule_refresh()
   end
 
-
-  defp node_children(node_list) do
-    case List.first(node_list) do
-      node when is_tuple(node) -> elem(node, 2)
-      _ -> nil
-    end
-  end
-
-  defp filter_nodes_by_tag_name(node_list, tag_name), do: node_list |> Enum.filter(
-    fn
-      node when is_tuple(node) -> elem(node, 0) != tag_name
-      node -> true
-    end
-  )
-  defp format_date(french_date, time) do
-    %{"number" => number, "month_name" => month_name} = Regex.named_captures(~r/[a-z]+ (?<number>\d+) (?<month_name>\D+)/, String.downcase(french_date))
-    [hours, minutes] = String.split(time, ":", parts: 2)
-    Timex.parse!("#{String.pad_leading(number, 2, "0")}/#{french_month_to_integer(month_name)}/#{Timex.today.year} #{String.pad_leading(hours, 2, "0")}:#{String.pad_leading(minutes, 2, "0")}", "%d/%m/%Y %H:%M", :strftime)
-  end
-
-  defp french_month_to_integer(french_month) do
-    months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
-    Enum.find_index(months, fn fm -> fm == french_month end) + 1
-    |> Integer.to_string
-    |> String.pad_leading(2, "0")
+  defp schedule_refresh() do
+    next_day_5_am = Timex.now() |> Timex.end_of_day() |> Timex.shift(hours: 5)
+    next_run_seconds_delay = Timex.diff(next_day_5_am, Timex.now(), :seconds)
+    Process.send_after(self(), :refresh, next_run_seconds_delay)
   end
 end
