306429ad94d56fa1ff207f14d4d31e2d49aa2b26
nresni <nresnikow@gmail.com>
2019-08-23 18:42:48 +0200

    [Application] Add Plug to application's access

---
+++ web/controllers/application_access.ex
@@ -0,0 +1,85 @@
+defmodule Vae.Plugs.ApplicationAccess do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Vae.Application
+  alias Vae.Repo
+  alias Vae.Router.Helpers
+
+  def init(_params) do
+  end
+
+  def call(%{params: %{"id" => application_id}} = conn, _params),
+    do: execute(conn, application_id)
+
+  def call(%{params: %{"application_id" => application_id}} = conn, _params),
+    do: execute(conn, application_id)
+
+  def call(conn, _params) do
+    conn
+    |> put_flash(:error, "Une erreur est survenue")
+    |> redirect(to: Helpers.root_path(conn, :index))
+    |> halt()
+  end
+
+  def execute(conn, application_id) do
+    application =
+      case Repo.get(Application, application_id) do
+        nil -> nil
+        application -> Repo.preload(application, :user)
+      end
+
+    hash = get_in(conn, [Access.key(:params), "hash"])
+
+    case has_access?(conn, application, hash) do
+      {:ok, nil} ->
+        conn
+        |> put_status(:not_found)
+        |> put_view(Vae.ErrorView)
+        |> render("404.html", layout: false)
+        |> halt()
+
+      {:ok, _application} ->
+        conn
+
+      {:error, %{to: to, msg: msg}} ->
+        conn
+        |> put_flash(:error, msg)
+        |> redirect(to: to)
+        |> halt()
+    end
+  end
+
+  defp has_access?(_conn, nil, _hash), do: {:ok, nil}
+
+  defp has_access?(conn, application, nil) do
+    if Coherence.logged_in?(conn) &&
+         (Coherence.current_user(conn).id == application.user.id ||
+            Coherence.current_user(conn).is_admin) do
+      {:ok, application}
+    else
+      {:error,
+       %{
+         to: Helpers.session_path(conn, :new, %{"mode" => "pe-connect"}),
+         msg: "Vous devez vous connecter"
+       }}
+    end
+  end
+
+  defp has_access?(conn, application, hash) do
+    # && Timex.before?(Timex.today, Timex.shift(application.delegate_access_refreshed_at, days: 10))
+    if application.delegate_access_hash == hash do
+      {:ok, application}
+    else
+      {:error,
+       %{
+         to: Helpers.root_path(conn, :index),
+         msg:
+           if(application.delegate_access_hash == hash,
+             do: "Accès expiré",
+             else: "Vous n'avez pas accès"
+           )
+       }}
+    end
+  end
+end
