4811ad4b980238d658843a27a9038464a2179b61
nresni <nresnikow@gmail.com>
2019-09-05 16:08:35 +0200

    [Meetings] Shutdown Afpa server

--- config/prod.exs
+++ config/prod.exs
@@ -29,10 +29,5 @@ config :vae, Vae.Scheduler,
           err -> Logger.error(fn -> inspect(err) end)
         end
       end
-    ],
-    afpa_refresh: [
-      timezone: "Europe/Paris",
-      schedule: "0 5 * * *",
-      task: &Vae.Delegates.refresh_afpa_meetings/0
     ]
   ]
