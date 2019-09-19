b8ff0d39b76d4e6a93bf5c4520c9acf902020d33
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-09-19 13:40:49 +0200

    improve dashboard tables

--- assets/js/admin/tables.js
+++ assets/js/admin/tables.js
@@ -3,18 +3,42 @@ import { render } from 'react-dom';
 
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import {CSVLink, CSVDownload} from 'react-csv';
+
 
 const mapColumnDefinitions = columns =>
   columns.map(columnName => ({
     accessor: columnName,
     Header: columnName,
     filterable: columnName.indexOf('name') > -1,
+    className: 'text-left',
   }));
 
 const mapRowDefinitions = (columns, rows) =>
   rows.map(row => row.reduce((obj, el, i) => {
     return Object.assign(obj, {[columns[i]]: el})
   }, {}));
+
+const displayRecordCount = state => {
+  const { filtered, pageRows, pageSize, sortedData, page } = state;
+
+  if (sortedData && sortedData.length > 0) {
+    const isFiltered = filtered.length > 0;
+    const totalRecords = sortedData.length;
+    const recordsCountFrom = page * pageSize + 1;
+    const recordsCountTo = recordsCountFrom + pageRows.length - 1;
+    return `${recordsCountFrom}-${recordsCountTo} of ${totalRecords} ${isFiltered ? 'filtered ' : ''}records`;
+  } else return "No records";
+}
+
+const makeTableWithCount = (state, makeTable) => {
+  return (
+    <div className="main-grid">
+      <p className="text-left">{displayRecordCount(state)}</p>
+      {makeTable()}
+    </div>
+  );
+}
 
 const renderTable = name => {
   const $table = document.getElementById(`${name}-table`);
@@ -23,15 +47,26 @@ const renderTable = name => {
       .then(res => res.json())
       .then(({columns, rows}) => {
         render(
-          <ReactTable
-            data={mapRowDefinitions(columns, rows)}
-            columns={mapColumnDefinitions(columns)}
-            multiSort={true}
-            defaultFilterMethod={(filter, row, column) => {
-              const id = filter.pivotId || filter.id
-              return String(row[id]).toLowerCase().indexOf(filter.value.toLowerCase()) > -1
-            }}
-          />
+          <div>
+            <div className="text-right">
+              <CSVLink
+                data={mapRowDefinitions(columns, rows)}
+                className="btn btn-primary"
+                style={{marginBottom: '1rem'}}
+              >Télécharger en CSV</CSVLink>
+            </div>
+            <ReactTable
+              data={mapRowDefinitions(columns, rows)}
+              columns={mapColumnDefinitions(columns)}
+              multiSort={true}
+              defaultFilterMethod={(filter, row, column) => {
+                const id = filter.pivotId || filter.id
+                return String(row[id]).toLowerCase().indexOf(filter.value.toLowerCase()) > -1
+              }}
+            >
+              {(state, makeTable) => makeTableWithCount(state, makeTable)}
+            </ReactTable>
+          </div>
         , document.getElementById(`${name}-table`));
       })
   }
--- web/controllers/admin/api_controller.ex
+++ web/controllers/admin/api_controller.ex
@@ -59,24 +59,21 @@ defmodule ExAdmin.ApiController do
   def delegates_query() do
     """
     select
-      q.certifier_name,
       q.delegate_name,
       q.total,
       q.submitted,
       (100 * q.submitted / NULLIF(total, 0)) as submitted_percent,
       q.admissible,
-      q.inadmissible,
+      q.inadmissible as not_yet_admissible,
       (q.admissible + q.inadmissible) * 100 / NULLIF(q.submitted, 0) as responded_percent,
       q.admissible * 100 / NULLIF(q.admissible + q.inadmissible, 0) as admissible_percent
     from (
-      select certifiers.name as certifier_name, delegates.name as delegate_name,
+      select delegates.name as delegate_name,
       (select count(*) from applications where applications.delegate_id = delegates.id) as total,
       (select count(*) from applications where applications.delegate_id = delegates.id  and applications.submitted_at IS NOT NULL) as submitted,
       (select count(*) from applications where applications.delegate_id = delegates.id  and applications.admissible_at IS NOT NULL) as admissible,
       (select count(*) from applications where applications.delegate_id = delegates.id  and applications.inadmissible_at IS NOT NULL) as inadmissible
       from delegates
-      inner join certifiers_delegates on delegates.id = certifiers_delegates.delegate_id
-      inner join certifiers on certifiers_delegates.certifier_id = certifiers.id
     ) q
     order by admissible_percent desc NULLS LAST, total desc
     """
@@ -85,7 +82,6 @@ defmodule ExAdmin.ApiController do
   def certifications_query() do
     """
     select
-      q.certifier_name,
       q.certification_name,
       q.total,
       q.submitted,
@@ -95,14 +91,12 @@ defmodule ExAdmin.ApiController do
       (q.admissible + q.inadmissible) * 100 / NULLIF(q.submitted, 0) as responded_percent,
       (100 * q.admissible / NULLIF(q.admissible + q.inadmissible, 0)) as admissible_percent
     from (
-      select certifiers.name as certifier_name, CONCAT(certifications.acronym, certifications.label) as certification_name,
+      select CONCAT(certifications.acronym, certifications.label) as certification_name,
       (select count(*) from applications where applications.certification_id = certifications.id) as total,
       (select count(*) from applications where applications.certification_id = certifications.id  and applications.submitted_at IS NOT NULL) as submitted,
       (select count(*) from applications where applications.certification_id = certifications.id  and applications.admissible_at IS NOT NULL) as admissible,
       (select count(*) from applications where applications.certification_id = certifications.id  and applications.inadmissible_at IS NOT NULL) as inadmissible
       from certifications
-      inner join certifier_certifications on certifications.id = certifier_certifications.certification_id
-      inner join certifiers on certifier_certifications.certifier_id = certifiers.id
     ) q
     order by admissible_percent desc NULLS LAST, total desc
     """
