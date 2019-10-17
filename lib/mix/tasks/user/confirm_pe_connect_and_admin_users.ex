8c2742b789ad51084cd19169bca1aeda3996030a
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-10-17 11:17:12 +0200

    fix mix task

--- lib/mix/tasks/user/confirm_pe_connect_and_admin_users.ex
+++ lib/mix/tasks/user/confirm_pe_connect_and_admin_users.ex
@@ -12,16 +12,11 @@ defmodule Mix.Tasks.User.ConfirmPEConnectAndAdminUsers do
   def run(_args) do
     ensure_started(Repo, [])
     {:ok, _started} = Application.ensure_all_started(:httpoison)
-    from(u in User, where: is_nil(u.confirmed_at) and not is_nil(u.pe_id))
-    |> Repo.all
-    |> Enum.each(fn user ->
-      Repo.update(User.changeset(user, %{confirmed_at: Timex.now()}))
-    end)
     from(u in User, where: is_nil(u.confirmed_at) and u.is_admin)
-    |> Repo.all
-    |> Enum.each(fn user ->
-      Repo.update(User.changeset(user, %{confirmed_at: Timex.now()}))
-    end)
+    |> Repo.update_all(set: [confirmed_at: Timex.now()])
+
+    from(u in User, where: is_nil(u.confirmed_at) and not is_nil(u.pe_id))
+    |> Repo.update_all(set: [confirmed_at: Timex.now()])
   end
 
 end
