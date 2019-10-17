2f0f4f67f7815ade2d61336578c766ea8feccee2
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-10-17 17:48:03 +0200

    flash message conditionnal

--- web/controllers/coherence/redirects.ex
+++ web/controllers/coherence/redirects.ex
@@ -46,7 +46,7 @@ defmodule Coherence.Redirects do
   # Uncomment the import below if adding overrides
   import Vae.Router.Helpers
 
-  alias Vae.{Application, Repo}
+  alias Vae.{Application, Repo, User}
 
   # Add function overrides below
 
@@ -121,19 +121,30 @@ defmodule Coherence.Redirects do
   def redirect_to_user_application(conn, user, application) do
     if application do
       conn
-        |> Phoenix.Controller.put_flash(
-            :success,
-            "Bienvenue sur votre page de candidature. Merci de compléter vos informations #{unless user.confirmed_at, do: "et confirmer votre adresse email "}avant de transmettre votre dossier au certificateur."
-          )
+        |> welcome_message_if_necessary(conn, user)
         |> redirect(to: application_path(conn, :show, application.id))
     else
       conn
-        |> Phoenix.Controller.put_flash(
-          :info,
-          "Vous êtes maintenant connecté !"
-        )
         |> redirect(to: root_path(conn, :index))
     end
+  end
+
+  def welcome_message_if_necessary(conn, user) do
+    todos = [
+      unless User.address(user), do: "compléter vos informations",
+      unless user.confirmed_at, do: "confirmer votre adresse email"
+    ] |> Enum.filter(&(&1))
+    if length(todos) > 0 do
+      message = "Bienvenue sur votre page de candidature. Merci de #{todos |> Enum.join(" et ")} avant de transmettre votre dossier au certificateur."
+
+      Phoenix.Controller.put_flash(conn, :success, message)
+    else
+      conn
+    end
+    base = "Bienvenue sur votre page de candidature."
+
+    base = unless User.address(user)), do: "#{base} Merci de compléter vos informations", else: base
+    base = unless user.confirmed_at, do:
   end
 
 end
