21a089d951ccbff5dd2dcfac20c651b0d086d81d
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-10-28 16:23:06 +0100

    fix

--- prod-server.sh
+++ prod-server.sh
@@ -3,8 +3,8 @@
 # mix deps.get --only prod && \
 # mix deps.compile && \
 # npm install --prefix ./assets && \
-npm run deploy --prefix ./assets && \
-mix do compile, phx.digest && \
+# npm run deploy --prefix ./assets && \
+# mix do compile, phx.digest && \
 # (cd deps/bcrypt_elixir && make clean && make) && \
 mix ecto.migrate && \
 mix phx.server
