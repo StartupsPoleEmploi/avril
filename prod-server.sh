6958510bd5e31c9fa7733c8503782aa2f12a0ea8
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-10-28 21:19:04 +0100

    docker modif

--- prod-server.sh
+++ prod-server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# mix deps.get --only prod && \
+mix deps.get --only prod && \
 # mix deps.compile && \
 # npm install --prefix ./assets && \
-# npm run deploy --prefix ./assets && \
-# mix do compile, phx.digest && \
-# (cd deps/bcrypt_elixir && make clean && make) && \
+npm run deploy --prefix ./assets && \
+mix phx.digest && \
+mix do compile && \
 mix ecto.migrate && \
 mix phx.server
