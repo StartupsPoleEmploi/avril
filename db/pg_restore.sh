ac6b9c6964eb4db18a09e0b4f61a8ed18e85dd31
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-10-29 18:03:14 +0100

    update script

--- db/pg_restore.sh
+++ db/pg_restore.sh
@@ -19,13 +19,15 @@ if createdb -h $POSTGRES_HOST -U $POSTGRES_USER -w $POSTGRES_DB; then
   echo "DB $POSTGRES_DB created";
 fi
 
-if psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c 'select count(*) from schema_migrations;'; then
+if ! psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c 'select count(*) from schema_migrations;'; then
   echo "FINISH: Database $POSTGRES_DB has migrations: no need to seed."
 else
   echo "Creating $POSTGRES_DB and seeding it"
 
   if [[ -f $DUMP_FILE ]]; then
     pg_restore --verbose --clean --create --no-acl --no-owner -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER -w $DUMP_FILE || true
+    echo "Checking restore"
+    psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c 'select count(*) from schema_migrations;' || true
     echo "FINISH: Database seeded"
   else
     echo "FINISH: Dump file $DUMP_FILE not found"
