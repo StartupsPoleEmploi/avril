7949237c5b09739629690bc8822bb339b304b129
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-10-29 15:13:20 +0100

    update script

--- db/pg_restore.sh
+++ db/pg_restore.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+export PGPASSWORD=$POSTGRES_PASSWORD
+
 DUMP_FILE="./latest.dump"
 
 createdb -h $POSTGRES_HOST -U $POSTGRES_USER -W $POSTGRES_DB
 
 if [[ -f $DUMP_FILE ]]; then
-  pg_restore --verbose --clean --create --no-acl --no-owner -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER $DUMP_FILE
+  pg_restore --verbose --clean --create --no-acl --no-owner -h $POSTGRES_HOST -d $POSTGRES_DB -U $POSTGRES_USER -W $DUMP_FILE
 else
   echo "Dump file $DUMP_FILE not found"
+  echo $(pwd)
 fi
