a2e3f647e3056d63407d32dd0d1a00a27e6d06a2
nresni <nresnikow@gmail.com>
2019-12-11 15:01:58 +0100

    [livret1] Rollback fix api callback end_date od proven experiences

--- web/models/proven_experience.ex
+++ web/models/proven_experience.ex
@@ -27,16 +27,15 @@ defmodule Vae.ProvenExperience do
   end
 
   def unique_key(experience) do
-    "#{experience.company_uid}-#{experience.label}-#{Vae.Date.format_for_unique_key(experience.start_date)}-#{Vae.Date.format_for_unique_key(experience.end_date)}"
+    "#{experience.company_uid}-#{experience.label}-#{
+      Vae.Date.format_for_unique_key(experience.start_date)
+    }-#{Vae.Date.format_for_unique_key(experience.end_date)}"
   end
 
   def experiencesprofessionellesdeclareesparlemployeur_api_map(api_fields) do
-    # If the experience is not finished, we receive '01/01/4000', which must be changed to nil
-    end_date = Vae.Date.format(api_fields["dateFin"])
-    end_date = if end_date && Date.compare(Timex.today(), end_date) == :gt, do: end_date
     %__MODULE__{
       start_date: Vae.Date.format(api_fields["dateDebut"]),
-      end_date: end_date,
+      end_date: Vae.Date.format(api_fields["dateFin"]),
       duration: api_fields["dureeContrat"],
       label: Vae.String.titleize(api_fields["intitulePoste"]),
       contract_type: api_fields["natureContrat"],
