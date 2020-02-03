c1f454c1dbba0af1834cca193125eb6f7f3f0b05
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-02-03 17:57:07 +0100

    restore edit rome profession backoffice

--- web/models/profession.ex
+++ web/models/profession.ex
@@ -14,19 +14,17 @@ defmodule Vae.Profession do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:label, :priority])
-    |> cast_assoc(:rome)
+    |> cast(params, [:label, :priority, :rome_id])
     |> slugify()
     |> validate_required([:label, :slug, :priority])
     |> unique_constraint(:label)
     |> unique_constraint(:slug)
-    |> assoc_constraint(:rome)
   end
 
   def format_for_index(struct) do
     struct
     |> Map.take(__schema__(:fields))
-    |> Map.put_new(:rome_code, struct.rome.code)
+    |> Map.put_new(:rome_code, get_in(Map.from_struct(struct), [:rome, :code]))
     |> Map.put_new(:length, String.length(struct.label))
     |> Map.put_new(:priority, struct.priority)
     |> Map.drop([:inserted_at, :updated_at])
