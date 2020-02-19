fd3f787ba5325b13f42d957222c8cc040231b534
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-02-19 13:06:42 +0100

    improve scripts deploy code change

---
+++ scripts/utils/deploy_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./deploy_local.sh --need-change avril-livret1 && \
+./deploy_local.sh --need-change avril
--- scripts/utils/deploy_local.sh
+++ scripts/utils/deploy_local.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-REPO_NAME=${1?"Usage: deploy <REPO_NAME> <BRANCH_NAME || master>"}
+if [ "$1" == "--need-change" ]; then
+  ONLY_IF_CHANGES=true;
+  shift;
+fi
+
+REPO_NAME=${1?"Usage: deploy_local.sh --need-change <REPO_NAME> <BRANCH_NAME || master>"}
 BRANCH_NAME=${2:-master}
+
+GIT_PULL_COMMAND="git pull origin $BRANCH_NAME"
+
+if [ "$ONLY_IF_CHANGES" = true ]; then
+  GIT_PULL_COMMAND="$GIT_PULL_COMMAND | grep -q -v 'up to date'"
+fi
 
 declare -A REPO_TO_SERVICE
 REPO_TO_SERVICE[avril]=phoenix
@@ -14,11 +25,15 @@ if test -z "$SERVICE_NAME"; then
   exit 1;
 fi
 
-echo "Pulling $BRANCH_NAME of $REPO_NAME to update $SERVICE_NAME"
+echo "Pulling $BRANCH_NAME of $REPO_NAME to deploy $SERVICE_NAME..."
 
 cd /home/docker/$REPO_NAME
 
 git checkout $BRANCH_NAME
-git pull origin $BRANCH_NAME
 
-../avril/scripts/utils/docker_update.sh $SERVICE_NAME
+if [[ $(eval $GIT_PULL_COMMAND) ]]; then
+  echo "Deploying $SERVICE_NAME..."
+  ../avril/scripts/utils/docker_update.sh $SERVICE_NAME
+else
+  echo "$REPO_NAME hasn't changed, no need to deploy $SERVICE_NAME"
+fi
