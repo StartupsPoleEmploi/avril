73cea18d2d695ef8e2e8869196e27e491120d0b7
nresni <nresnikow@gmail.com>
2020-03-24 09:34:16 +0100

    Oooops !

---
+++ lib/vae_web/views/user_application_view.ex
@@ -0,0 +1,7 @@
+defmodule VaeWeb.UserApplicationView do
+  use VaeWeb, :view
+  use Scrivener.HTML
+
+  def format_meeting_date(meeting), do: Timex.format!(meeting.start_date, "%d/%m/%Y", :strftime)
+  def format_meeting_hour(meeting), do: Timex.format!(meeting.start_date, "%Hh%M", :strftime)
+end
