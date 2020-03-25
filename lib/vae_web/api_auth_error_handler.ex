039080fa73bbedbe31066064ba809e6ff24807fe
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-03-25 17:54:58 +0100

    restore redirect to sign in route api error

--- lib/vae_web/api_auth_error_handler.ex
+++ lib/vae_web/api_auth_error_handler.ex
@@ -1,11 +1,18 @@
 defmodule VaeWeb.APIAuthErrorHandler do
   use VaeWeb, :controller
   alias Plug.Conn
+  alias VaeWeb.Router.Helpers, as: Routes
 
   @spec call(Conn.t(), :not_authenticated) :: Conn.t()
   def call(conn, :not_authenticated) do
     conn
     |> put_status(401)
-    |> json(%{error: %{code: 401, message: "Not authenticated"}})
+    |> json(%{
+      error: %{
+        code: 401,
+        message: "Not authenticated",
+        redirect_to: Routes.pow_session_url(conn, :new)
+      }
+    })
   end
 end
