41ac87a1ca432721c88beed66986f7cbc55c78cf
nresni <nresnikow@gmail.com>
2020-03-31 14:26:22 +0200

    [Admin] Fix rome associations

--- lib/vae_web/admin/rome.ex
+++ lib/vae_web/admin/rome.ex
@@ -15,22 +15,22 @@ defmodule Vae.ExAdmin.Rome do
 
     show rome do
       attributes_table do
-        row :id
+        row(:id)
         row(:category, fn r -> Vae.Rome.category(r) |> Helpers.link_to_resource() end)
         row(:subcategory, fn r -> Vae.Rome.subcategory(r) |> Helpers.link_to_resource() end)
-        row :code
-        row :label
-        row :url
+        row(:code)
+        row(:label)
+        row(:url)
       end
 
       panel "Certifications" do
-        table_for application.certifications do
+        table_for rome.certifications do
           column(:label, fn r -> Helpers.link_to_resource(r) end)
         end
       end
 
       panel "Professions" do
-        table_for application.professions do
+        table_for rome.professions do
           column(:label, fn r -> Helpers.link_to_resource(r, namify: fn p -> p.label end) end)
         end
       end
