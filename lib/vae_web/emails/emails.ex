ae23cdb07d1a5b7adad9cf212fd928ce395bd4ed
Nicolas Resnikow <nresnikow@gmail.com>
2020-04-29 00:02:10 +0200

    [Api] Submit application

--- lib/vae_web/emails/emails.ex
+++ lib/vae_web/emails/emails.ex
@@ -8,7 +8,7 @@ defmodule VaeWeb.Emails do
     if Delegate.is_asp?(application.delegate) do
       send_asp_application_submission_confirmation(application)
     else
-      send_user_and_delegate_meeting_confirmation(application)
+      send_user_and_delegate_submission_confirmation(application)
     end
   end
 
@@ -18,14 +18,20 @@ defmodule VaeWeb.Emails do
     |> Mailer.send()
   end
 
-  defp send_user_and_delegate_meeting_confirmation(application) do
+  defp send_asp_application_submission_confirmation(application) do
+    application
+    |> ApplicationEmail.asp_user_submission_confirmation()
+    |> Mailer.send()
+  end
+
+  defp send_user_and_delegate_submission_confirmation(application) do
     with {:user, {:ok, _sent}} <-
-           send_user_meeting_confirmation(application),
+           send_user_submission_confirmation(application),
          {:delegate, {:ok, _sent}} <-
-           send_delegate_application_submission_and_meeting_confirmation(application) do
+           send_delegate_application_submission_confirmation(application) do
       {:ok, application}
     else
-      {:error, {error, message}} ->
+      {key, {error, message}} when key in [:user, :delegate] ->
         Logger.error(fn -> inspect(error, limit: :infinity) end)
         {:error, message}
 
@@ -34,15 +40,17 @@ defmodule VaeWeb.Emails do
     end
   end
 
-  defp send_asp_application_submission_confirmation(application) do
-    application
-    |> ApplicationEmail.asp_user_submission_confirmation()
-    |> Mailer.send()
+  defp send_delegate_application_submission_confirmation(application) do
+    {:delegate,
+     application
+     |> ApplicationEmail.delegate_submission()
+     |> Mailer.send()}
   end
 
-  defp send_delegate_application_submission_and_meeting_confirmation(application) do
-    application
-    |> ApplicationEmail.delegate_submission()
-    |> Mailer.send()
+  defp send_user_submission_confirmation(application) do
+    {:user,
+     application
+     |> ApplicationEmail.user_submission_confirmation()
+     |> Mailer.send()}
   end
 end
