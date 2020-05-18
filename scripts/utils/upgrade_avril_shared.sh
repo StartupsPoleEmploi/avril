89e9a8fa22e069b07a5bfcda4ca417f26c966d6c
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-18 11:35:43 +0200

    upgrade avril-shared works

--- scripts/utils/upgrade_avril_shared.sh
+++ scripts/utils/upgrade_avril_shared.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-COMMIT_MESSAGE="upgrade avril-shared"
-
 yarn_upgrade() {
   SERVICE_NAME=$1
   ASSET_FOLDER=${2:-.}
-  UPGRADE_COMMAND="cd $ASSET_FOLDER && yarn upgrade avril"
-  docker-compose exec $SERVICE_NAME bash -c $UPGRADE_COMMAND
+  docker-compose exec $SERVICE_NAME bash -c "cd $ASSET_FOLDER && yarn upgrade avril"
 }
 
 git_commit_upgrade() {
   ASSET_FOLDER=${1:-.}
-  GIT_FOLDER=${2:-.}
-  if [[ -n "$GIT_FOLDER" ]]
-    then
-      GIT_DIR_OPTION="--git-dir=../$GIT_FOLDER/.git"
-    else
-      # atom ~/.bashrc
+  GIT_FOLDER=${2:-avril}
+  if [[ -n "$GIT_FOLDER" ]] ; then
+    GIT_DIR_OPTION="--git-dir=../$GIT_FOLDER/.git";
   fi
   git $GIT_DIR_OPTION add $ASSET_FOLDER/yarn.lock
-  git $GIT_DIR_OPTION commit -m $COMMIT_MESSAGE
+  git $GIT_DIR_OPTION commit -m "upgrade avril-shared"
   git $GIT_DIR_OPTION push
 }
 
 services=(
-  phoenix,assets,.
+  phoenix,assets
   nuxt_profile,.,avril-profil
-  nuxt_booklet,.,avril-booklet
+  nuxt_booklet,.,avril-livret1
 )
 for service in "${services[@]}"; do
-  IFS=',' read name asset git <<< "${i}"
+  IFS=',' read name asset git <<< "${service}"
   echo "Executing $name with $asset and $git"
-  # yarn_upgrade $name $asset
-  # git_commit_upgrade $asset $git
+
+  yarn_upgrade $name $asset
+  git_commit_upgrade $asset $git
 done
