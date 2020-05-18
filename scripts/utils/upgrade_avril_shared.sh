7248e31f36b6bfbc68722bce0f0aff32ea910222
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-18 11:24:23 +0200

    scripts

--- scripts/utils/upgrade_avril_shared.sh
+++ scripts/utils/upgrade_avril_shared.sh
@@ -1,3 +1,36 @@
-#!/bins/h
+#!/bin/bash
 
-docker-compose exec phoenix bash -c "cd assets && yarn upgrade avril"
+COMMIT_MESSAGE="upgrade avril-shared"
+
+yarn_upgrade() {
+  SERVICE_NAME=$1
+  ASSET_FOLDER=${2:-.}
+  UPGRADE_COMMAND="cd $ASSET_FOLDER && yarn upgrade avril"
+  docker-compose exec $SERVICE_NAME bash -c $UPGRADE_COMMAND
+}
+
+git_commit_upgrade() {
+  ASSET_FOLDER=${1:-.}
+  GIT_FOLDER=${2:-.}
+  if [[ -n "$GIT_FOLDER" ]]
+    then
+      GIT_DIR_OPTION="--git-dir=../$GIT_FOLDER/.git"
+    else
+      # atom ~/.bashrc
+  fi
+  git $GIT_DIR_OPTION add $ASSET_FOLDER/yarn.lock
+  git $GIT_DIR_OPTION commit -m $COMMIT_MESSAGE
+  git $GIT_DIR_OPTION push
+}
+
+services=(
+  phoenix,assets,.
+  nuxt_profile,.,avril-profil
+  nuxt_booklet,.,avril-booklet
+)
+for service in "${services[@]}"; do
+  IFS=',' read name asset git <<< "${i}"
+  echo "Executing $name with $asset and $git"
+  # yarn_upgrade $name $asset
+  # git_commit_upgrade $asset $git
+done
