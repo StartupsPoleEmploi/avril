0d3c13311ed3634fef27787521b730493090dd15
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-20 11:05:49 +0200

    update deploy local

--- scripts/utils/deploy_local.sh
+++ scripts/utils/deploy_local.sh
@@ -26,12 +26,12 @@ echo "Pulling $BRANCH_NAME of $REPO_NAME to deploy $SERVICE_NAME..."
 
 cd /home/docker/$REPO_NAME
 
+git fetch --all
+
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ "$CURRENT_BRANCH" != "$BRANCH_NAME" ]; then
   git checkout $BRANCH_NAME
 fi
-
-git pull origin $BRANCH_NAME
 
 HEADHASH=$(git rev-parse HEAD)
 UPSTREAMHASH=$(git rev-parse master@{upstream})
@@ -39,6 +39,9 @@ UPSTREAMHASH=$(git rev-parse master@{upstream})
 if [[ "$DEPLOY_IF_CHANGE" == "true" && "$HEADHASH" == "$UPSTREAMHASH" ]]; then
   echo "$REPO_NAME hasn't changed, no need to deploy $SERVICE_NAME"
 else
+  git reset --hard origin/$BRANCH_NAME
+  # git pull origin $BRANCH_NAME
+
   echo "Deploying $SERVICE_NAME..."
   ../avril/scripts/utils/docker_update.sh $SERVICE_NAME
 fi
