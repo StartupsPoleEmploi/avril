7b6a5f503b8e04e5f11491fa0de0ec5a22a91791
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-26 17:39:16 +0200

    fix parameterize oe

--- lib/vae/helpers/string.ex
+++ lib/vae/helpers/string.ex
@@ -40,6 +40,7 @@ defmodule Vae.String do
   def parameterize(string, separator) do
     string
     |> :unicode.characters_to_nfd_binary()
+    |> String.replace("œ", "oe")
     |> String.replace(~r/[\\"\-]/u, " ")
     |> String.replace(~r/[^A-z\s0-9]/u, "")
     |> String.downcase()
