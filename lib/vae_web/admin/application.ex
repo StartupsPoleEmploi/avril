9fb6cd19c79f6d4e9c23a92d90d45a89c0270162
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-06-11 17:59:29 +0200

    delegate mode check cerfa

--- lib/vae_web/admin/application.ex
+++ lib/vae_web/admin/application.ex
@@ -32,7 +32,7 @@ defmodule Vae.ExAdmin.UserApplication do
 
       href =
         VaeWeb.Router.Helpers.user_application_path(VaeWeb.Endpoint, :show, application,
-          hash: application.delegate_access_hash
+          delegate_hash: application.delegate_access_hash
         )
 
       action_item_link("View Delegate Application", href: href, target: "_blank")
@@ -63,7 +63,7 @@ defmodule Vae.ExAdmin.UserApplication do
 
       if application.booklet_1 do
         action_item_link("Check CERFA",
-          href: Vae.UserApplication.booklet_url(VaeWeb.Endpoint, application, [path: "/cerfa"]),
+          href: Vae.UserApplication.booklet_url(VaeWeb.Endpoint, application, [path: "/cerfa", delegate_mode: true]),
           target: "_blank"
         )
       end
