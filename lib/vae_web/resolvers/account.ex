d98d023f3fb835daa0b90de6af64bb34374a1d89
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-11-23 11:23:27 +0100

    orthographe

--- lib/vae_web/resolvers/account.ex
+++ lib/vae_web/resolvers/account.ex
@@ -3,7 +3,7 @@ defmodule VaeWeb.Resolvers.Account do
 
   alias Vae.Account
 
-  @update_profile_error "Erreur de mise à jour du profile"
+  @update_profile_error "Erreur de mise à jour du profil"
   @update_password_error "Erreur lors de la mise à jour du mot de passe"
 
   def identity_item(_, _, %{context: %{current_user: user}}) do
