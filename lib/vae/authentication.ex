b614a86b8d952d0e1d88e619038b7a5bccf8f253
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-12-03 09:51:43 +0100

    fix username

--- lib/vae/authentication.ex
+++ lib/vae/authentication.ex
@@ -1,9 +1,10 @@
 defimpl ExAdmin.Authentication, for: Plug.Conn do
   alias VaeWeb.Router.Helpers, as: Routes
+  alias Vae.User
 
   def use_authentication?(_), do: true
   def current_user(conn), do: Pow.Plug.current_user(conn)
-  def current_user_name(conn), do: Pow.Plug.current_user(conn).name
+  def current_user_name(conn), do: User.fullname(Pow.Plug.current_user(conn))
   def session_path(conn, :delete), do: Routes.logout_path(conn, :delete)
   def session_path(conn, action), do: Routes.login_path(conn, action)
 end
