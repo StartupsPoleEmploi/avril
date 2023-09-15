b5f084bee6e1aa4d4513fb9455ba99bc3a521838
Augustin Riedinger <augustin.riedinger@gmail.com>
2023-09-15 17:21:48 +0200

    fix insert

--- lib/vae/authorities/rncp/update.ex
+++ lib/vae/authorities/rncp/update.ex
@@ -47,10 +47,14 @@ defmodule Vae.Authorities.Rncp.Update do
     |> Certification.rncp_update()
   end
 
-  defp insert_fiche(%{rncp_id: rncp_id} = fiche) do
+  defp insert_fiche(fiche) do
+    %{rncp_id: rncp_id} = params = Vae.Authorities.Rncp.FicheHandler.api_fiche_to_certification_params(fiche)
+
     if !Repo.get_by(Certification, rncp_id: rncp_id) do
-      update_fiche(fiche)
-      case fiche[:older_certification] do
+      rncp_id
+      |> Certification.rncp_changeset(params)
+      |> Certification.rncp_update()
+      case params[:older_certification] do
         %Certification{rncp_id: rncp_id} -> update_certification(rncp_id)
       _ -> nil
       end
